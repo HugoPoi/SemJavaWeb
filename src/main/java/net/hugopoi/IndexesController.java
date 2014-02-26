dff1f08afbd82d28bfaf058a38bb33219a02f3fb
HugoPoi <hugo.poissonnet@gmail.com>
2014-02-26 11:08:27 +0100

    Velocity Commit

--- src/main/java/net/hugopoi/IndexesController.java
+++ src/main/java/net/hugopoi/IndexesController.java
@@ -3,16 +3,23 @@ package net.hugopoi;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Hashtable;
+import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 public class IndexesController extends HttpServlet {
 
@@ -24,19 +31,42 @@ public class IndexesController extends HttpServlet {
 	protected Boolean autoIndex = false;
 	protected String staticRoot = ".";
 	
+	protected Template indexesTemplate = null;
+	
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		autoIndex = Boolean.parseBoolean(config.getInitParameter("autoindex"));
 		staticRoot = config.getInitParameter("static-root");
+		Properties p = new Properties();
+		p.setProperty("file.resource.loader.path", config.getServletContext().getRealPath("/"));
+		Velocity.init(p);
 	}
 	
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		
+		indexesTemplate = Velocity.getTemplate("indexes.vm");
+		String cutUrl = request.getPathInfo();
+		if(cutUrl == null){
+			cutUrl = "";
+		}
+		System.out.println(cutUrl);
 		
-		String cutUrl = request.getRequestURI().split("/",4)[3];
-		Path path = Paths.get(this.staticRoot+"/"+cutUrl);
+		Path path = null;
+		try {
+			path = Paths.get(this.staticRoot+"/"+ new java.net.URI(cutUrl).getPath());
+		} catch (URISyntaxException e) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		String parenturl = request.getRequestURL().substring(0, request.getRequestURL().lastIndexOf("/"))+"/";
 		
 		if(!Files.isDirectory(path)){
+			if(request.getParameter("delete") != null){
+				path.toFile().delete();
+				response.sendRedirect(parenturl);
+				return;
+			}
 			response.setContentType(Files.probeContentType(path));
 			byte buffer[] = new byte[16384];
 			OutputStream outbin = response.getOutputStream();
@@ -47,15 +77,25 @@ public class IndexesController extends HttpServlet {
 			inbin.close();
 			outbin.close();
 		}else if (Files.isDirectory(path) && autoIndex) {
-			request.setAttribute("title", "Mon Titre" );
-			request.setAttribute("items", Files.newDirectoryStream(path));
-			String parenturl = request.getRequestURL().substring(0, request.getRequestURL().lastIndexOf("/"))+"/";
+			VelocityContext vcontext = new VelocityContext();
+			vcontext.put("title",path.toAbsolutePath().toString());
+			DirectoryStream<Path> test = Files.newDirectoryStream(path);
+			
+//			Hashtable<String, Path> itemlist = new Hashtable<>();
+//			for (Path path2 : test) {
+//				itemlist.put(path2.getFileName().toString(), path2);
+//			}
+			
+			vcontext.put("items", test.iterator());
+			vcontext.put("currenturl", request.getRequestURL());
 			System.out.println(request.getRequestURL().toString());
 			System.out.println(parenturl);
+			
 			if(!parenturl.equals(request.getRequestURL().toString())){
-				request.setAttribute("parenturl",  parenturl);
+				vcontext.put("parenturl",  parenturl);
 			}
-			request.getRequestDispatcher("/indexes.jsp").include(request, response);
+			indexesTemplate.merge(vcontext, response.getWriter());
+			return;
 		}else{
 			response.sendError(HttpServletResponse.SC_FORBIDDEN);
 		}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World!<%= new java.util.Date() %> hesjkfhksjhdk</h2>
-</body>
-</html>
